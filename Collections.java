fdd4126c0cc426fa5c5a4b5a8d5787598f2070fe
SHIVAM SINGH <91870115+shivam8989@users.noreply.github.com>
2023-03-21 19:13:47 +0530

    Add files via upload
    implementation of navigable set

---
+++ Collections.java
@@ -0,0 +1,14 @@
+package Binary_Tree;
+import java.util.*;
+public class Collections {
+    public static void main(String args[]){
+        NavigableSet<Integer> list = new TreeSet<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
+        
+        }
+    }
+
